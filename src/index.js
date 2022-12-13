d6df4f3d0eaaa61f9f337aa2d4f6ead9e693ff50
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2022-12-13 20:08:46 +0200

    task 1 without fixing

---
+++ src/index.js
@@ -0,0 +1,101 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+const searchField = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchField.addEventListener('input', onSearch);
+
+function onSearch(event) {
+  event.preventDefault();
+  const input = event.currentTarget;
+  const searchQuery = input.value.trim();
+
+  function fetchCountry(name) {
+    return fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    ).then(response => {
+      return response.json();
+    });
+  }
+
+  if (searchQuery === '') {
+    cleanCountryList();
+    cleanCountryInfo();
+  }
+
+  fetchCountry(searchQuery).then(data => {
+    if (data.length > 10) {
+      return alert(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    }
+
+    if (data.length >= 2 && data.length < 10) {
+      createCountryList(data);
+      cleanCountryInfo();
+      return;
+    }
+
+    if (data.length === 1) {
+      cleanCountryList();
+      createCountryInfo(data);
+      return;
+    }
+  });
+}
+
+function createCountryList(data) {
+  countryList.innerHTML = createMarkupCountryList(data);
+}
+
+function createCountryInfo(data) {
+  countryInfo.innerHTML = createMarkupCountryInfo(data);
+}
+
+function cleanCountryList() {
+  countryList.innerHTML = '';
+}
+
+function cleanCountryInfo() {
+  countryInfo.innerHTML = '';
+}
+
+function createMarkupCountryList(data) {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li>
+          <img src="${flags.svg}" alt="flag" width="30px"/>
+          <h2>${name.common}</h2>
+        </li>`
+    )
+    .join('');
+}
+
+function createMarkupCountryInfo(data) {
+  return data
+    .map(
+      ({ name, capital, population, flags, languages }) =>
+        `<div class="info">
+          <img src="${flags.svg}" alt="flag" width="100px"/>
+          <h1>${name.common}</h1>
+        </div>
+        <div>
+          <p class="title"><b>Capital:</b> ${capital}</p>
+          <p class="title"><b>Population:</b> ${population}</p>
+          <p class="title"><b>Languages:</b> ${Object.values(languages)}</p>
+        </div>`
+    )
+    .join('');
+}
+
+// function fetchCountry(name) {
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+//   ).then(response => {
+//     return response.json;
+//   });
+// }
