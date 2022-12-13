f9fa9a8ae135912b5ed4abe4dd83caa90fdf2c82
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2022-12-13 20:52:24 +0200

    task 1 with adding

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './css/styles.css';
+import { fetchCountry } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -6,45 +9,39 @@ const searchField = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-searchField.addEventListener('input', onSearch);
+searchField.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
   event.preventDefault();
   const input = event.currentTarget;
   const searchQuery = input.value.trim();
 
-  function fetchCountry(name) {
-    return fetch(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    ).then(response => {
-      return response.json();
-    });
-  }
+  fetchCountry(searchQuery)
+    .then(data => {
+      if (searchQuery === '') {
+        cleanCountryList();
+        cleanCountryInfo();
+      }
 
-  if (searchQuery === '') {
-    cleanCountryList();
-    cleanCountryInfo();
-  }
+      if (data.length > 10) {
+        return Notify.failure(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
 
-  fetchCountry(searchQuery).then(data => {
-    if (data.length > 10) {
-      return alert(
-        'Too many matches found. Please enter a more specific name.'
-      );
-    }
+      if (data.length >= 2 && data.length < 10) {
+        createCountryList(data);
+        cleanCountryInfo();
+        return;
+      }
 
-    if (data.length >= 2 && data.length < 10) {
-      createCountryList(data);
-      cleanCountryInfo();
-      return;
-    }
-
-    if (data.length === 1) {
-      cleanCountryList();
-      createCountryInfo(data);
-      return;
-    }
-  });
+      if (data.length === 1) {
+        cleanCountryList();
+        createCountryInfo(data);
+        return;
+      }
+    })
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
 }
 
 function createCountryList(data) {
@@ -91,11 +88,3 @@ function createMarkupCountryInfo(data) {
     )
     .join('');
 }
-
-// function fetchCountry(name) {
-//   return fetch(
-//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-//   ).then(response => {
-//     return response.json;
-//   });
-// }
